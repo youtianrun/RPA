f17a0ffc4ce1cfb3a7bba9ecf586adce3993db63
YTR <56672533+youtianrun@users.noreply.github.com>
2025-07-05 22:19:40 +0800

    Update page3.py

--- pages/page3.py
+++ pages/page3.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import networkx as nx
-import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 from pyvis.network import Network
 import tempfile
